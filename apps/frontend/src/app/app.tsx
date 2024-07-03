ddb92e35f1e8197568d4b4b491672875263099f0
akshay upadhya <akshaysrinagar@gmail.com>
2024-07-03 19:36:29 +0530

    (chore): adding nestjs

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,49 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
+import { Login } from './Login';
+import { Signup } from './Signup';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend2" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/login">Home</Link>
           </li>
           <li>
-            <Link to="/page-2">Page 2</Link>
+            <Link to="/signup">Signup</Link>
           </li>
         </ul>
       </div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<span>here</span>} />
+        <Route path="/login" Component={Login} />
+        <Route path="/signup" Component={Signup} />
       </Routes>
       {/* END: routes */}
     </div>
